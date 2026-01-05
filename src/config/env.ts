c00fc75adf89c1d21437855de59151bb391629c6
Lucas Gabriel <eslucasgabriel@gmail.com>
2026-01-05 16:21:41 -0300

    update config env whit bd values

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,14 @@
 import 'dotenv/config';
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3333;
-
 export const env = {
-  port,
-  nodeEnv: process.env.NODE_ENV ?? 'development',
+  port: Number(process.env.PORT) || 3333,
+  nodeEnv: process.env.NODE_ENV || 'development',
+
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER || 'root',
+    pass: process.env.DB_PASS || '',
+    name: process.env.DB_NAME || 'api_rest',
+  },
 };
